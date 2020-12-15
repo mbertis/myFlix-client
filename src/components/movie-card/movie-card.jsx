124506a2915a34b25639157cc827339d23355424
mbertis <madisonbertis@gmail.com>
2020-12-15 11:36:23 -0800

    added styling to movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
 
 export class MovieCard extends React.Component {     //Exporting <MovieCard/> here allows it to be imported into 'MainView'
   render() {
@@ -7,7 +9,14 @@ export class MovieCard extends React.Component {     //Exporting <MovieCard/> he
     const { movie, onClick } = this.props;
 
     return (
-      <div onClick = {() => onClick(movie)} className = "movie-card">{movie.Title}</div>
+      <Card style={{width: '16rem'}}>
+        <Card.Img variant="top" src={movie.ImagePath}/>
+        <Card.Body>
+          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Text>{movie.Description}</Card.Text>
+          <Button onClick={() => onClick(movie)} variant="link">Open</Button>
+        </Card.Body>
+      </Card>
     );
   }
 }
